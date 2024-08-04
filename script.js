d37ded69966d5eeb177d3154506122e6e743fd61
RoCoKo <muhammetrasitozgul@gmail.com>
2024-08-04 21:14:59 +0300

    Update countries

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Constants for pagination
-let PAGE_SIZE = 10;  // Default page size
+let PAGE_SIZE = 20;
 let currentPage = 1;
 let totalPages = 0;
 let leaderboardData = [];
 
-// XP required to advance from one level to the next
 const xpToNextLevel = [
     15, 20, 25, 40, 65, 100, 155, 225, 315, 435, 600, 800, 1075, 1415, 1835, 2345,
     2950, 3650, 4425, 5300, 6300, 7350, 8525, 9785, 11150, 12600, 14200, 15850,
@@ -19,7 +17,6 @@ const xpToNextLevel = [
     5000000000, 6000000000
 ];
 
-// Calculate cumulative XP for each level
 const cumulativeXP = [];
 let totalXP = 0;
 for (let i = 0; i < xpToNextLevel.length; i++) {
@@ -33,13 +30,13 @@ function getLevel(xp) {
             return i + 2;
         }
     }
-    return 0; // Level 0 if XP is below the first level's requirement
+    return 0;
 }
 
 function formatRelativeTime(timestamp) {
     const now = Date.now();
     const lastOnlineDate = new Date(timestamp * 1000);
-    const diff = now - lastOnlineDate.getTime(); // Difference in milliseconds
+    const diff = now - lastOnlineDate.getTime();
 
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const months = Math.floor(days / 30);
@@ -58,62 +55,122 @@ function formatRelativeTime(timestamp) {
     }
 }
 
-// Location mapping
 const locationMap = {
+    1: 'Austria',
     2: 'Germany',
     3: 'Turkey',
     7: 'Italy',
-    13: 'France',
-    19: 'Finland',
-    21: 'Russia',
     12: 'Spain',
+    13: 'France',
     14: 'The Netherlands',
+    16: 'Ireland',
     17: 'Hungary',
     18: 'Norway',
+    19: 'Finland',
+    20: 'Iceland',
+    21: 'Russia',
     22: 'Switzerland',
     23: 'United Kingdom',
     24: 'Portugal',
     25: 'Greece',
+    28: 'Belgium',
+    33: 'Denmark',
     34: 'Poland',
     35: 'Czech Republic',
+    36: 'Slovakia',
+    38: 'Croatia',
+    39: 'Bosnia and Herzegovina',
+    44: 'Estonia',
+    45: 'Latvia',
     46: 'Belarus',
     47: 'Ukraine',
+    48: 'Moldova',
     49: 'Romania',
+    50: 'Bulgaria',
     51: 'Sweden',
     52: 'Georgia',
     54: 'Azerbaijan',
     55: 'Cyprus',
+    56: 'Lithuania',
     58: 'Canada',
     61: 'Brazil',
     62: 'Argentina',
+    63: 'Bolivia',
     64: 'Colombia',
+    66: 'Guiana',
+    67: 'Paraguay',
+    68: 'Peru',
+    70: 'Uruguay',
+    71: 'Venezuela',
+    72: 'French Guiana',
     73: 'Chile',
+    74: 'Alaska',
+    75: 'Greenland',
     76: 'Hawaii',
+    79: 'California',
+    80: 'Nevada',
+    83: 'Arizona',
+    84: 'Montana',
     86: 'Colorado',
+    91: 'Kansas',
     92: 'Oklahoma',
     93: 'Texas',
     92: 'Oklahoma',
+    100: 'Illinois',
     105: 'Tennessee',
+    113: 'Rhode Island',
     114: 'New York',
     116: 'New Jersey',
     124: 'South Carolina',
+    125: 'Georgia (USA)',
     126: 'Florida',
     127: 'Mexico',
+    128: 'Bahamas',
+    130: 'Dominican Republic',
+    131: 'Haiti',
+    132: 'Cuba',
+    138: 'Panama',
+    141: 'Victoria',
     143: 'Western Australia',
+    144: 'Northern Territory',
     145: 'South Australia',
     146: 'Queensland',
     147: 'Tasmania',
     148: 'New South Wales',
     149: 'New Zealand',
+    151: 'Oman',
+    153: 'Saudi Arabia',
+    159: 'Iraq',
+    166: 'Pakistan',
     167: 'Afghanistan',
+    172: 'Kazakhstan',
     173: 'Nepal',
     174: 'India',
     177: 'Myanmar',
     178: 'Thailand',
+    180: 'Laos',
+    182: 'Malaysia',
+    183: 'Brunei',
     184: 'Philippines',
     186: 'China',
+    187: 'North Korea',
+    188: 'South Korea',
+    189: 'Japan',
+    201: 'Ivory Coast',
+    205: 'Togo',
+    212: 'Egypt',
     216: 'Gabon',
-    225: 'Belarus'
+    219: 'Democratic Republic of the Congo',
+    221: 'Burundi',
+    222: 'Angola',
+    223: 'Zambia',
+    225: 'Belarus',
+    226: 'South Africa',
+    227: 'Zimbabwe',
+    230: 'Madagascar',
+    232: 'Uganda',
+    234: 'Somalia',
+    239: 'Swaziland',
 };
 
 function updateTable(page) {
@@ -125,7 +182,7 @@ function updateTable(page) {
     const pageData = leaderboardData.slice(start, end);
 
     pageData.forEach((player, index) => {
-        const rank = start + index + 1; // Calculate rank
+        const rank = start + index + 1;
         const lastOnlineRelative = formatRelativeTime(player.last_ping_time);
         const level = getLevel(player.xp);
         const location = locationMap[player.location_id] || 'Unknown';
@@ -177,7 +234,6 @@ async function loadLeaderboard() {
             }
         });
 
-        // Event listeners for page size buttons
         document.querySelector('#pageSize10').addEventListener('click', () => {
             changePageSize(10);
             document.querySelectorAll('.page-size-buttons button').forEach(btn => btn.classList.remove('active'));
